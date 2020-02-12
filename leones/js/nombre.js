f1e6853119844d5f8b904fe6703b50d7a9feca9c
Pablo Rebaque <pablo.rebaque.diaz@ericsson.com>
2020-02-12 23:19:28 +0100

    Remove moto
    Seems more clear

--- leones/js/nombre.js
+++ leones/js/nombre.js
@@ -12,12 +12,6 @@ images['Vampirina'] = 'vampirina.jpg';
 images['Los Pitufos'] = 'pitufos.jpg'
 images['La Pantera Rosa'] = 'pantera.jpg'
 
-const moto = {};
-moto['Lady Bug'] = "¡Puntos fuera!";
-moto['Vampirina'] = "¡Soy como tú!";
-moto['Los Pitufos'] = "¡Pitufeando!";
-moto['La Pantera Rosa'] = "¿Por qué los humanos no pueden ser tan inteligentes como los animales?";
-
 function setName(name) {
     $('#nombre').text(name);
 }
@@ -26,16 +20,10 @@ function setImage(table) {
     $('#mesa').attr('src', 'img/'+images[table]);
 }
 
-function setMoto(table) {
-    $('#moto').text(moto[table]);
-}
-
-
 
 $(function() {
     const name = decodeURIComponent(getUrlVars()['nombre']);
     const table = decodeURIComponent(getUrlVars()['mesa']);
     setName(name);
     setImage(table);
-    setMoto(table);
 });
